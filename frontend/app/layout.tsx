6cd068b13c40cbc281f6e4ada8e1d2e2f1aa24c5
SHOSAN-Git <info@sho-san.co.jp>
2025-11-05 17:15:58 +0900

    Delete frontend directory

--- frontend/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "BlockNote Repro" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif" }}>{children}</body>
-    </html>
-  );
-}
