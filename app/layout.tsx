4876cd0967f6496339ae1efe34e92fb4b71f85fb
SHOSAN-Git <info@sho-san.co.jp>
2025-11-05 16:47:37 +0900

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "BlockNote Repro" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
-}
