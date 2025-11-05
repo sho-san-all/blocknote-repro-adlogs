3a11c85739505fc2b501ef227a4c735208beabd9
SHOSAN-Git <info@sho-san.co.jp>
2025-11-05 16:20:20 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "BlockNote Repro" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
